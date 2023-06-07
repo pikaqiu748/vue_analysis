6711f6e22265d6a5e497b71f6cd4bf061dc7b178
hectorhli <hectorhli@tencent.com>
2023-06-07 15:04:31 +0800

    添加构建文件注释

--- vue源码/scripts/config.js
+++ vue源码/scripts/config.js
@@ -9,24 +9,19 @@ const version = process.env.VERSION || require('../package.json').version
 const weexVersion = process.env.WEEX_VERSION || require('../packages/weex-vue-framework/package.json').version
 const featureFlags = require('./feature-flags')
 
-const banner =
-  '/*!\n' +
-  ` * Vue.js v${version}\n` +
-  ` * (c) 2014-${new Date().getFullYear()} Evan You\n` +
-  ' * Released under the MIT License.\n' +
-  ' */'
+const banner = '/*!\n' + ` * Vue.js v${version}\n` + ` * (c) 2014-${new Date().getFullYear()} Evan You\n` + ' * Released under the MIT License.\n' + ' */'
 
 const weexFactoryPlugin = {
-  intro () {
+  intro() {
     return 'module.exports = function weexFactory (exports, document) {'
   },
-  outro () {
+  outro() {
     return '}'
-  }
+  },
 }
 
 const aliases = require('./alias')
-const resolve = p => {
+const resolve = (p) => {
   const base = p.split('/')[0]
   if (aliases[base]) {
     return path.resolve(aliases[base], p.slice(base.length + 1))
@@ -42,14 +37,14 @@ const builds = {
     dest: resolve('dist/vue.runtime.common.dev.js'),
     format: 'cjs',
     env: 'development',
-    banner
+    banner,
   },
   'web-runtime-cjs-prod': {
     entry: resolve('web/entry-runtime.js'),
     dest: resolve('dist/vue.runtime.common.prod.js'),
     format: 'cjs',
     env: 'production',
-    banner
+    banner,
   },
   // Runtime+compiler CommonJS build (CommonJS)
   'web-full-cjs-dev': {
@@ -58,7 +53,7 @@ const builds = {
     format: 'cjs',
     env: 'development',
     alias: { he: './entity-decoder' },
-    banner
+    banner,
   },
   'web-full-cjs-prod': {
     entry: resolve('web/entry-runtime-with-compiler.js'),
@@ -66,14 +61,14 @@ const builds = {
     format: 'cjs',
     env: 'production',
     alias: { he: './entity-decoder' },
-    banner
+    banner,
   },
   // Runtime only ES modules build (for bundlers)
   'web-runtime-esm': {
     entry: resolve('web/entry-runtime.js'),
     dest: resolve('dist/vue.runtime.esm.js'),
     format: 'es',
-    banner
+    banner,
   },
   // Runtime+compiler ES modules build (for bundlers)
   'web-full-esm': {
@@ -81,7 +76,7 @@ const builds = {
     dest: resolve('dist/vue.esm.js'),
     format: 'es',
     alias: { he: './entity-decoder' },
-    banner
+    banner,
   },
   // Runtime+compiler ES modules build (for direct import in browser)
   'web-full-esm-browser-dev': {
@@ -91,7 +86,7 @@ const builds = {
     transpile: false,
     env: 'development',
     alias: { he: './entity-decoder' },
-    banner
+    banner,
   },
   // Runtime+compiler ES modules build (for direct import in browser)
   'web-full-esm-browser-prod': {
@@ -101,7 +96,7 @@ const builds = {
     transpile: false,
     env: 'production',
     alias: { he: './entity-decoder' },
-    banner
+    banner,
   },
   // runtime-only build (Browser)
   'web-runtime-dev': {
@@ -109,7 +104,7 @@ const builds = {
     dest: resolve('dist/vue.runtime.js'),
     format: 'umd',
     env: 'development',
-    banner
+    banner,
   },
   // runtime-only production build (Browser)
   'web-runtime-prod': {
@@ -117,7 +112,7 @@ const builds = {
     dest: resolve('dist/vue.runtime.min.js'),
     format: 'umd',
     env: 'production',
-    banner
+    banner,
   },
   // Runtime+compiler development build (Browser)
   // 入口文件为 web/entry-runtime-with-compiler.js，最终输出 dist/vue.js，
@@ -127,7 +122,7 @@ const builds = {
     format: 'umd',
     env: 'development',
     alias: { he: './entity-decoder' },
-    banner
+    banner,
   },
   // Runtime+compiler production build  (Browser)
   'web-full-prod': {
@@ -136,14 +131,14 @@ const builds = {
     format: 'umd',
     env: 'production',
     alias: { he: './entity-decoder' },
-    banner
+    banner,
   },
   // Web compiler (CommonJS).
   'web-compiler': {
     entry: resolve('web/entry-compiler.js'),
     dest: resolve('packages/vue-template-compiler/build.js'),
     format: 'cjs',
-    external: Object.keys(require('../packages/vue-template-compiler/package.json').dependencies)
+    external: Object.keys(require('../packages/vue-template-compiler/package.json').dependencies),
   },
   // Web compiler (UMD for in-browser use).
   'web-compiler-browser': {
@@ -152,7 +147,7 @@ const builds = {
     format: 'umd',
     env: 'development',
     moduleName: 'VueTemplateCompiler',
-    plugins: [node(), cjs()]
+    plugins: [node(), cjs()],
   },
   // Web server renderer (CommonJS).
   'web-server-renderer-dev': {
@@ -160,14 +155,14 @@ const builds = {
     dest: resolve('packages/vue-server-renderer/build.dev.js'),
     format: 'cjs',
     env: 'development',
-    external: Object.keys(require('../packages/vue-server-renderer/package.json').dependencies)
+    external: Object.keys(require('../packages/vue-server-renderer/package.json').dependencies),
   },
   'web-server-renderer-prod': {
     entry: resolve('web/entry-server-renderer.js'),
     dest: resolve('packages/vue-server-renderer/build.prod.js'),
     format: 'cjs',
     env: 'production',
-    external: Object.keys(require('../packages/vue-server-renderer/package.json').dependencies)
+    external: Object.keys(require('../packages/vue-server-renderer/package.json').dependencies),
   },
   'web-server-renderer-basic': {
     entry: resolve('web/entry-server-basic-renderer.js'),
@@ -175,19 +170,19 @@ const builds = {
     format: 'umd',
     env: 'development',
     moduleName: 'renderVueComponentToString',
-    plugins: [node(), cjs()]
+    plugins: [node(), cjs()],
   },
   'web-server-renderer-webpack-server-plugin': {
     entry: resolve('server/webpack-plugin/server.js'),
     dest: resolve('packages/vue-server-renderer/server-plugin.js'),
     format: 'cjs',
-    external: Object.keys(require('../packages/vue-server-renderer/package.json').dependencies)
+    external: Object.keys(require('../packages/vue-server-renderer/package.json').dependencies),
   },
   'web-server-renderer-webpack-client-plugin': {
     entry: resolve('server/webpack-plugin/client.js'),
     dest: resolve('packages/vue-server-renderer/client-plugin.js'),
     format: 'cjs',
-    external: Object.keys(require('../packages/vue-server-renderer/package.json').dependencies)
+    external: Object.keys(require('../packages/vue-server-renderer/package.json').dependencies),
   },
   // Weex runtime factory
   'weex-factory': {
@@ -195,14 +190,14 @@ const builds = {
     entry: resolve('weex/entry-runtime-factory.js'),
     dest: resolve('packages/weex-vue-framework/factory.js'),
     format: 'cjs',
-    plugins: [weexFactoryPlugin]
+    plugins: [weexFactoryPlugin],
   },
   // Weex runtime framework (CommonJS).
   'weex-framework': {
     weex: true,
     entry: resolve('weex/entry-framework.js'),
     dest: resolve('packages/weex-vue-framework/index.js'),
-    format: 'cjs'
+    format: 'cjs',
   },
   // Weex compiler (CommonJS). Used by Weex's Webpack loader.
   'weex-compiler': {
@@ -210,55 +205,84 @@ const builds = {
     entry: resolve('weex/entry-compiler.js'),
     dest: resolve('packages/weex-template-compiler/build.js'),
     format: 'cjs',
-    external: Object.keys(require('../packages/weex-template-compiler/package.json').dependencies)
-  }
+    external: Object.keys(require('../packages/weex-template-compiler/package.json').dependencies),
+  },
 }
 
-function genConfig (name) {
+function genConfig(name) {
+  // builds是不同环境下的基本配置
   const opts = builds[name]
   const config = {
+    // 配置文件入口
     input: opts.entry,
+    // 用来声明某些依赖是再外部引用的，这些依赖不会打包进来。
+    // 例如在打包的代码中，引入了jQuery，但是上下文中有全局的jQuery，
+    //  这时打包的过程就可以使用该字段来排除打包jQuery。
     external: opts.external,
-    plugins: [
-      flow(),
-      alias(Object.assign({}, aliases, opts.alias))
-    ].concat(opts.plugins || []),
+    // rollup使用的插件
+    plugins: [flow(), alias(Object.assign({}, aliases, opts.alias))].concat(opts.plugins || []),
     output: {
+      // 生成的文件名及路径
       file: opts.dest,
+      // 生成代码的格式，例如MAD，CJS，ES等
       format: opts.format,
+      // 文件头部添加的内容
       banner: opts.banner,
-      name: opts.moduleName || 'Vue'
+      // 如果代码是一个库，指定库的名字
+      name: opts.moduleName || 'Vue',
     },
+    // 用于拦截警告信息，即拿到警告信息，可以做一些判断或者处理
     onwarn: (msg, warn) => {
       if (!/Circular/.test(msg)) {
         warn(msg)
       }
-    }
+    },
   }
 
   // built-in vars
+  // Weex 是一套简单易用的跨平台开发方案，能以 web 的开发体验构建高性能、可扩展的 native 应用，
+  // 为了做到这些，Weex 与 Vue 合作，使用 Vue 作为上层框架，并遵循 W3C 标准实现了统一的 JSEngine 和 DOM API，
+  // 这样一来，你甚至可以使用其他框架驱动 Weex，打造三端一致的 native 应用。
+  // 就是说 Weex 是一个壳，定义了一些 协议，可以使用其他 framework 来实现它。
+  // Web 的开发体验，native 的体验。
+
   const vars = {
     __WEEX__: !!opts.weex,
     __WEEX_VERSION__: weexVersion,
-    __VERSION__: version
+    __VERSION__: version,
   }
   // feature flags
-  Object.keys(featureFlags).forEach(key => {
+  Object.keys(featureFlags).forEach((key) => {
+    // 向vars变量上挂载属性
     vars[`process.env.${key}`] = featureFlags[key]
   })
   // build-specific env
   if (opts.env) {
+    // 设置NODE_ENV值，如果有的话
     vars['process.env.NODE_ENV'] = JSON.stringify(opts.env)
   }
+  // plugins是个数组
   config.plugins.push(replace(vars))
 
+  // 判断是否有转译插件
+  // 在平日的印象中，似乎 Webpack 就等于 bundling，就如官方文档首页写的那样 "bundle your ..."，
+  // 看上去 Webpack 就是来做 bundling 的。在浏览器上，bundles 确实很不错，
+  // 能够以非常合理的策略加载和执行。但在 NodeJS 中，事情就变得有点不同了。
+  // 在 NodeJS 中，以分文件的方式组织和执行 JS 文件通常是更自然的实践，
+  // 主要是因为源文件路径相关的逻辑有时是避免不了的，
+  // 比如：在云函数或 Serverless 中注册事件、在某些 CLI 辅助下生成的源代码。
+  // 然而，想要在这基础上添加服务端渲染（SSR）逻辑，考虑到前端部分可能已经用 Webpack 引用了各式各样的文件，
+  // 后端部分可能也不得不用 Webpack 做 bundling。但这也意味着，我们不得不做相当量的工作来让 bundles
+  // 可以合理适配这些源文件路径相关的逻辑。
+  // 为了更轻松的在 NodeJS 中使用 Webpack，Transpile Webpack Plugin(transpile-webpack-plugin) 便诞生了。
+  // 这个插件能够将 entry 引用到的全部文件列为输入文件，经过编译，再按照相同的目录格式输出到输出目录中。
   if (opts.transpile !== false) {
     config.plugins.push(buble())
   }
 
   Object.defineProperty(config, '_name', {
     enumerable: false,
-    value: name
+    value: name,
   })
 
   return config
